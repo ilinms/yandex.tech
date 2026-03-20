aee5dbea741e7b21313793538ce59dac3c96e4e1
matvei_ilin_1 <ilinms.3007@mail.ru>
2026-03-20 16:39:16 +0300

    remove __pycache__ from repo

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DISK_OAUTH_TOKEN = os.getenv("DISK_OAUTH_TOKEN")
-DISK_BASE_URL = os.getenv("DISK_BASE_URL", "https://cloud-api.yandex.net/v1/disk")
-TEST_FOLDER = "/test_automation"
